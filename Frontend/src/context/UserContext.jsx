6b8aa66fc9a979260592112d7a5621035f57e3cc
Sunidhi-source <sunidhisharma1208@gmail.com>
2026-03-16 20:07:44 +0530

    Fixing context import and cleaning up files

--- Frontend/src/context/UserContext.jsx
+++ Frontend/src/context/UserContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState } from 'react';
 
-export const UserContext = createContext();
+const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
     const [user, setUser] = useState(null);
