ce48c3385ba22d3aa5f5951986e59d962c4dd741
Sunidhi-source <sunidhisharma1208@gmail.com>
2026-03-16 20:17:05 +0530

    Fix: matching import case with filename

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -12,7 +12,7 @@ import Income from './pages/Dashboard/Income';
 import Expense from './pages/Dashboard/Expense';
 import Profile from './pages/Dashboard/Profile';
 
-import UserProvider from './context/userContext';
+import UserProvider from './context/UserContext';
 import { Toaster } from 'react-hot-toast';
 
 function App() {
